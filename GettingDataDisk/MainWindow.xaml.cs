8f69291d0eb6eeba15ddf5880b9e7727f7c76b5e
Gostev Konstantin <47049219+Cyclist-code@users.noreply.github.com>
2021-02-11 13:55:38 +0700

    Adding logic to output disk content

--- GettingDataDisk/MainWindow.xaml.cs
+++ GettingDataDisk/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.IO;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -30,6 +31,44 @@ namespace GettingDataDisk
         private void CloseMethod(object sender, EventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void MainWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            foreach(var drive in drives)
+            {
+                SelectionDisk.Items.Add(drive);
+            }
+        }
+
+        private void SelectionDisk_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DisplayContentDisk();
+        }
+
+        private void DisplayContentDisk()
+        {
+            ContentDisk.Text = "";
+            try
+            {
+                var drive = SelectionDisk.SelectedItem.ToString();
+                string[] dirs = Directory.GetDirectories(drive);
+                foreach(string s in dirs)
+                {
+                    ContentDisk.Text += s.Replace(drive, "") + Environment.NewLine;
+                }
+
+                string[] files = Directory.GetFiles(drive);
+                foreach(string s in files)
+                {
+                    ContentDisk.Text += s.Replace(drive, "") + Environment.NewLine;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Диск не найден!", "Нет диска", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
